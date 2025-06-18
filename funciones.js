b95652c454fb9379531dfd2261bd2b13e829de71
Gonzalo Sartori <tucorreo@gmail.com>
2025-06-18 05:54:45 -0300

    Primer commit

---
+++ funciones.js
@@ -0,0 +1,5 @@
+function mostrarImagenOriginal() {
+  const imagen = document.getElementById("mueble-img");
+  imagen.src = "foto-mueble-original.jpg"; // Asegúrate de tener esta imagen
+  imagen.alt = "Foto original del mueble";
+}
