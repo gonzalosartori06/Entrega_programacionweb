d6726667d80bd657cf5b061dfd563f32f8e3b16d
Gonzalo Sartori <tucorreo@gmail.com>
2025-06-19 17:58:29 -0300

    11

--- funciones.js
+++
@@ -1,5 +0,0 @@
-function mostrarImagenOriginal() {
-  const imagen = document.getElementById("mueble-img");
-  imagen.src = "foto-mueble-original.jpg"; // Asegúrate de tener esta imagen
-  imagen.alt = "Foto original del mueble";
-}
